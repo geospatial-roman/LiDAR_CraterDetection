70b4a9e5db5649d814c7399899880b9b1e5bdfdc
RomanPCML <26280268+RomanPCML@users.noreply.github.com>
2019-09-24 22:51:35 +0200

    Add files via upload

--- preprocessing/append_labels.py
+++ preprocessing/append_labels.py
@@ -1,23 +1,25 @@
-from shapely.geometry import Point, Polygon
+
 import numpy as np
-import geopandas
 import matplotlib.path as mpltPath
 import datetime
 import pandas as pd
+from utils import read_shapes
+
+
 
 
 
 def label_Classes(input, shapes, column_name, output):
-    '''
+    """
     This function append the "Label" Column to the input Pointcloud (.txt) by checking which points are within the given polygons
     seperator in input txt : tabulator
     Columns must be Named (X,Y,Z,...)
-    :param input: pointclout (.txt)
+    :param input: point cloud (.txt)
     :param shapes: polygons (.shp)
     :param column_name:
     :param output: new pointcloud (.txt)
     :return:
-    '''
+    """
 
     pc_txt = input
     crater_poly = shapes
@@ -27,197 +29,117 @@ def label_Classes(input, shapes, column_name, output):
     pc = pd.read_csv(pc_txt, sep='\t')
     pc = pc.rename(columns = {'//X': 'X'})
     print(datetime.datetime.now(), 'Starting with File: ', pc_txt)
-    shapes = geopandas.read_file(crater_poly)
+    shapes = read_shapes.read_shapefile(crater_poly)
     pointList = []
 
     # read polygon shape file and save as a list of geometries
-    g = [i for i in shapes.geometry]
-    l = [row[column] for index, row in shapes.iterrows()]
-    polys = []
-    count = 0
-    for i in range(len(g)):
-        count += 1
-        x,y = g[i].exterior.coords.xy
-        coords = np.dstack((x,y)).tolist()
-        polys.append([coords, l[i]])
+    polys = [[row['coords'],row[column]] for index, row in shapes.iterrows()]
 
-    #create list of all points in 2D
-    for index,row in pc.iterrows():
+    # create list of all points in 2D
+    for index, row in pc.iterrows():
         x= row['X']
         y= row['Y']
-        pointList.append([x,y])
+        pointList.append([x, y])
 
-    #check if points are in polygons and append 0-1 lables to df
+    # check if points are in polygons and append 0-1 lables to df
     resultsList = []
     labelList = [0] * len(pointList)
     labelArray = np.array(labelList)
     for poly in polys:
 
         label = poly[1]
-        path = mpltPath.Path(poly[0][0])
-        single_result = path.contains_points(pointList)
-        single_array = np.array(single_result)
-        resultsList.append(single_array*label)
-        #labelList = [label if single_result[x] == True else labelList[x] for x in range(len(single_result))]
-        '''
-        for i in range(len(single_result)):
-            if single_array[i] == True:
-                labelArray[i] = label
-        '''
-    #labelArray = np.array(labelList)
-    resultsArray = np.array(resultsList)
-    final_results = sum([array for array in resultsArray])
-    pc['Label'] = final_results
-
-    print(pc.head())
-
-    # removes higher values from overlapping polygons
-    #pc.loc[pc['Label'] > 1,'Label'] = 1
-
-
-    pc.to_csv(output, sep='\t')
-    print('done')
-
-
-
-def label_IDs(input, shapes, output):
-    '''
-    This function append the "Label" Column to the input Pointcloud (.txt) by checking which points are within the given polygons
-    seperator in input txt : tabulator
-    Columns must be Named (X,Y,Z,...)
-    :param input: pointclout (.txt)
-    :param shapes: polygons (.shp)
-    :param column_name:
-    :param output: new pointcloud (.txt)
-    :return:
-    '''
-
-    pc_txt = input
-    crater_poly = shapes
-    output = output
-    #column = column_name
-
-    pc = pd.read_csv(pc_txt, sep='\t')
-    pc = pc.rename(columns = {'//X': 'X'})
-    print(datetime.datetime.now(), 'Starting with File: ', pc_txt)
-    shapes = geopandas.read_file(crater_poly)
-    pointList = []
-
-    # read polygon shape file and save as a list of geometries
-    g = [i for i in shapes.geometry]
-    #l = [row[column] for index, row in shapes.iterrows()]
-    polys = []
-    count = 0
-    for i in range(len(g)):
-        count += 1
-        x,y = g[i].exterior.coords.xy
-        coords = np.dstack((x,y)).tolist()
-        polys.append(coords)
-
-    #create list of all points in 2D
-    for index,row in pc.iterrows():
-        x= row['X']
-        y= row['Y']
-        pointList.append([x,y])
-
-    #check if points are in polygons and append 0-1 lables to df
-    resultsList = []
-    labelList = [0] * len(pointList)
-    labelArray = np.array(labelList)
-    count = 0
-    for poly in polys:
-
-        label = count
         path = mpltPath.Path(poly[0])
         single_result = path.contains_points(pointList)
         single_array = np.array(single_result)
         resultsList.append(single_array*label)
-        #labelList = [label if single_result[x] == True else labelList[x] for x in range(len(single_result))]
-        '''
-        for i in range(len(single_result)):
-            if single_array[i] == True:
-                labelArray[i] = label
-        '''
-    #labelArray = np.array(labelList)
+
+    # labelArray = np.array(labelList)
     resultsArray = np.array(resultsList)
     final_results = sum([array for array in resultsArray])
     pc['Label'] = final_results
     print(pc.head())
 
     # removes higher values from overlapping polygons
-    #pc.loc[pc['Label'] > 1,'Label'] = 1
+    pc.loc[pc['Label'] > 1, 'Label'] = 1
 
     pc.to_csv(output, sep='\t')
     print('done')
 
 
 def label_Ones(input, shapes, output):
-    '''
+    """
     This function append the "Label" Column to the input Pointcloud (.txt) by checking which points are within the given polygons
     seperator in input txt : tabulator
     Columns must be Named (X,Y,Z,...)
-    :param input: pointclout (.txt)
+    :param input: point cloud (.txt)
     :param shapes: polygons (.shp)
-    :param output: new pointcloud (.txt)
+    :param output: new point cloud (.txt)
     :return:
-    '''
+    """
 
     pc_txt = input
     crater_poly = shapes
     output = output
 
-    pc = pd.read_csv(pc_txt, sep='\t')
-    pc = pc.rename(columns = {'//X': 'X'})
+    pc = pd.read_csv(pc_txt, sep=';')
+    if 'Scalar field' in pc.columns:
+        pc = pc.drop(labels=['Scalar field'], axis=1)
+    if 'Label' in pc.columns:
+        pc = pc.drop(labels=['Label'], axis=1)
+    pc = pc.rename(columns={'//X': 'X'})
+    pc = pc.rename(columns={'//X': 'X', 'Scalar field #2': 'Gaussian curvature (3)'})
     print(datetime.datetime.now(), 'Starting with File: ', pc_txt)
-    shapes = geopandas.read_file(crater_poly)
+    shapes = read_shapes.read_shapefile(crater_poly)
     pointList = []
+    print(pc.head())
+
+    if 'Longitude' in shapes.columns:
+        shapes = shapes.rename(columns={'Longitude': 'X'})
+    if 'Lon' in shapes.columns:
+        shapes = shapes.rename(columns={'Lon': 'X'})
+    if 'Latitude' in shapes.columns:
+        shapes = shapes.rename(columns={'Latitude': 'Y'})
+    if 'Lat' in shapes.columns:
+        shapes = shapes.rename(columns={'Lat': 'X'})
+    if '//X' in shapes.columns:
+        shapes = shapes.rename(columns={'//X': 'X'})
 
     # read polygon shape file and save as a list of geometries
-    g = [i for i in shapes.geometry]
-    polys = []
-    count = 0
-    for i in range(len(g)):
-        count += 1
-        x,y = g[i].exterior.coords.xy
-        coords = np.dstack((x,y)).tolist()
-        polys.append([coords, 1])
+    polys = [[row['coords'], 1] for index, row in shapes.iterrows()]
 
-    #create list of all points in 2D
+    # create list of all points in 2D
     for index,row in pc.iterrows():
-        x= row['X']
-        y= row['Y']
-        pointList.append([x,y])
+        x = row['X']
+        y = row['Y']
+        pointList.append([x, y])
 
-    #check if points are in polygons and append 0-1 lables to df
+    # check if points are in polygons and append 0-1 labels to df
     resultsList = []
     labelList = [0] * len(pointList)
     labelArray = np.array(labelList)
     for poly in polys:
 
         label = poly[1]
-        path = mpltPath.Path(poly[0][0])
+        path = mpltPath.Path(poly[0])
         single_result = path.contains_points(pointList)
         single_array = np.array(single_result)
         resultsList.append(single_array*label)
-        #labelList = [label if single_result[x] == True else labelList[x] for x in range(len(single_result))]
-        '''
-        for i in range(len(single_result)):
-            if single_array[i] == True:
-                labelArray[i] = label
-        '''
-    #labelArray = np.array(labelList)
+
     resultsArray = np.array(resultsList)
     final_results = sum([array for array in resultsArray])
     pc['Label'] = final_results
 
-    print(pc.head())
-
     # removes higher values from overlapping polygons
-    pc.loc[pc['Label'] > 1,'Label'] = 1
+    pc.loc[pc['Label'] > 1, 'Label'] = 1
 
-    #pc = pc.drop([pc.columns[0]], axis = 1)
-    print(pc.columns.values)
-    pc = pc.drop([pc.columns[0]], axis = 1)
-    pc.to_csv(output, sep='\t')
+    # pc = pc.drop([pc.columns[0]], axis = 1)
+    if '//X' in pc.columns:
+        pc = pc.rename(columns={'//X': 'X'})
+    if pc.columns[0] != 'X':
+        pc = pc.drop([pc.columns[0]], axis = 1)
+
+    pc = pc.dropna(how='any', axis=0)
+    print('output columns: ', pc.columns.values)
+    pc.to_csv(output, sep=';', index=False)
     print('done')
 
